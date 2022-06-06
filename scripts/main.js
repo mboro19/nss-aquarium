4697de2cbb8ba40475f48c2ad2690e1e34d66227
David Stewart <mboro19@gmail.com>
2022-06-06 11:36:46 -0500

    some style changes were made to background

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,7 @@ for (const fish of allFish) {
 }
 
 // Import the FishList function from the correct module
-import {} from ''
+import { FishList } from './FishList.js'
 
 /*
     What is the CSS selector for the element where you
